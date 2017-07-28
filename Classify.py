1b3d24cfb02762f16969ae8113a0341e16b173ee
wangyukai <wyk747@126.com>
2017-07-28 17:00:57 +0800

    修改读取数据类

---
+++ Classify.py
@@ -0,0 +1,3 @@
+import nltk
+
+nltk.NaiveBayesClassifier.classify()
--- ReadData.py
+++ ReadData.py
@@ -4,27 +4,44 @@ import re
 
 class ReadData(object):
 
-    def readData(self, trainNumber, testNumber):
-        data = xlrd.open_workbook('spam.xls')
-        table = data.sheets()[0]
-        nrows = table.nrows
+    def __init__(self):
+        self.thu = thulac.thulac(filt=True)
+
+    def readData(self):
+        table, nrows = self.readRowNum()
         dataList = []
         for i in range(nrows):
            d = table.row_values(i)
+           filteUseless = self.replace(d[1])
+           #cutedWords = self.cut(filteUseless)
+           #d.append(cutedWords)
            dataList.append(d)
+        return dataList
 
-        return dataList[trainNumber:], dataList[:testNumber]
+    def readFile(self):
+        data = xlrd.open_workbook('spam.xls')
+        return data
 
-def replace(line):
-    regex = re.compile("[^\\u4e00-\u9fa5a-zA-z0-9]")
-    return regex.sub("", line)
+    def readRowNum(self):
+        data = self.readFile()
+        table = data.sheets()[0]
+        nrows = table.nrows
+        return table, nrows
 
-text = '您好！我是航城南门兴达置业的小熊，买卖房子流程不清楚的可以咨询我，祝您及您的家人万福安康！万福安康！万福安康！重要的事情说三遍！'
-text.strip().replace("\\pP|\\pS", "")
-print(replace(text))
-thu = thulac.thulac(filt=True, seg_only=True)
-result = thu.cut(text)
-print(result)
+    def replace(self, line):
+        regex = re.compile("[^\\u4e00-\u9fa5a-zA-z0-9]")
+        return regex.sub("", line)
+
+    def cut(self, text):
+        result = self.thu.cut(text)
+        return result
+
+    def dealData(self, trNumber, teNumber):
+        dataSet = []
+        data = self.readData()
+        
 
 
 
+trainData, testData = ReadData().readData(100, 100)
+print(trainData)
